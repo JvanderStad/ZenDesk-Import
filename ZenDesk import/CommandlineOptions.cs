9fc8d33b92b43af4d48de0c55074e962836ec3b9
Jeffrey van der Stad <jeffrey@hybridsaas.com>
2015-06-30 12:07:37 +0200

    Initial commit

---
+++ ZenDesk import/CommandlineOptions.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+
+namespace ZenDesk_import
+{
+	internal class CommandlineOptions
+	{
+		[Option("xml", HelpText = "Source Xml file")]
+		public string XmlFile { get; set; }
+
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this,
+			  current => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
---
+++ ZenDesk import/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml;
+using CommandLine;
+using NLog;
+
+namespace ZenDesk_import
+{
+	static class Program
+	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		static void Main(string[] args)
+		{
+			Logger.Info( "Starting application" );
+			var options = new CommandlineOptions();
+			var arguments = Parser.Default.ParseArguments(args, options);
+			if (!arguments)
+			{
+				Logger.Error("Error parsing parameters: {0}", String.Join(", ", args));
+				return;
+			}
+
+			if ( String.IsNullOrEmpty( options.XmlFile ) )
+			{
+				Logger.Error("Xml file not set");
+				return;
+			}
+
+
+			LoadXml(options.XmlFile);
+
+	
+			Console.ReadLine();
+		}
+
+		private static void LoadXml( string xmlFile )
+		{
+			Logger.Info( "Loading Xml" );
+
+			var xml = new XmlDocument();
+			xml.Load( xmlFile );
+		}
+	}
+}
